b5bcc9f266d8d8a82b3d2a491c29abece8d633c6
James Ward <james@jamesward.org>
2013-10-19 09:55:05 -0600

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+WebJar for NProgress
+
+More info: http://webjars.org
+
+Upstream: https://github.com/rstacruz/nprogress
---
+++ src/main/resources/webjars-requirejs.js
@@ -0,0 +1,7 @@
+/*global requirejs */
+
+requirejs.config({
+  shim: {
+    'nprogress': [ 'webjars!jquery.js' ]
+  }
+});
